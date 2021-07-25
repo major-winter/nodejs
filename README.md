888492643201f59ca6c21c9c3871c4f862375701
major_winter <jason@major.major>
2021-07-25 15:35:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Node Simple Chat App
+- This repo is a simple chat app written in NodeJs. The application makes use of the EventSource API and EventEmitter object inorder to achieve a functionality of transmitting message between 2 clients
---
+++ app.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const express = require('express')
+const EventEmitter = require('events')
+
+const port = process.env.PORT || 3000
+const app = express()
+const chatEmitter = new EventEmitter()
+
+function respondText(req,res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end("Hi")
+}
+
+function respondJson(req,res){
+  res.json({text: 'hi', numbers: [1, 2, 3]})
+}
+
+function respondNotFound(req,res){
+  res.writeHead(404, {'Content-Type': 'text/plain'})
+  res.end('Not Found')
+}
+
+function respondEcho(req, res) {
+  const {input = ''} = req.query
+
+  res.json({
+      normal: input,
+      shouty: input.toUpperCase(),
+      characterCount: input.length,
+      backwards: input.split('').reverse().join('')
+  })
+}
+
+function respondStatic (req, res) {
+  const filename = `${__dirname}/public/${req.params[0]}`
+  fs.createReadStream(filename).on('error', () => respondNotFound(req,res)).pipe(res)
+}
+
+function respondChat (req, res) {
+  const { message } = req.query
+
+  chatEmitter.emit('message', message)
+  res.end()
+}
+
+function respondSSE (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive'
+  })
+
+  const onMessage = msg => res.write(`data: ${msg}\n\n`)
+  chatEmitter.on('message', onMessage)
+
+  res.on('close', function () {
+    chatEmitter.off('message', onMessage)
+  })
+}
+
+app.get('/', respondText)
+app.get('/json', respondJson)
+app.get('/echo', respondEcho)
+app.get('/static/*', respondStatic)
+app.get('/chat', respondChat)
+app.get('/sse', respondSSE)
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
